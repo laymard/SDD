4d4efaeeb94b058d33d2eb1b55092fb4edaee4fa
laymard <Laurent.Aymard@insa-rennes.fr>
2015-01-17 02:15:58 +0100

    recherche question 4, l'attribut privé de racine de Arbre gene, erreur dans le sujet ?

---
+++ 2014 EX2/src/Visiteur.java
@@ -0,0 +1,43 @@
+
+public class Visiteur implements VisiteurArbre {
+
+	@Override
+	public void visitArbre(Arbre a) {
+		a.
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitNoeud(Noeud n) {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitNoeudBinaire(NoeudBinaire n) {
+		if (n.filsD.getClass()==NoeudValeur.class)
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitNoeudValeur(NoeudValeur n) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitNoeudPlus(NoeudPlus n) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visitNoeudMoins(NoeudMoins n) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 2014 EX2/src/VisiteurArbre.java
@@ -0,0 +1,9 @@
+
+public interface VisiteurArbre {
+	public void visitArbre(Arbre a  );
+	public void visitNoeud(Noeud n);
+	public void visitNoeudBinaire (NoeudBinaire n);
+	public void visitNoeudValeur (NoeudValeur n);
+	public void visitNoeudPlus (NoeudPlus n);
+	public void visitNoeudMoins (NoeudMoins n);
+}
